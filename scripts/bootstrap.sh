32c8517a9539a1f3f31dee092e1458b61c2c8c81
Zihan Hu <zihan-hu@foxmail.com>
2023-06-03 20:15:13 +0800

    chore: 添加 ESLint、prettier

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,5 @@
 pnpm install
 
 pnpm husky install
-pnpm husky add .husky/commit-msg 'npx commitlint --edit $1'
+pnpm husky add .husky/commit-msg 'pnpm commitlint --edit $1'
+pnpm husky add .husky/lint 'pnpm eslint . --fix'
